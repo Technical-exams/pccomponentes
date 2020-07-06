7380324aaeef8884ac8ed2706cd09c2eea344475
Temple <el.temple@gmail.com>
2020-07-06 19:14:16 +0200

    [PHPUNIT] Added new tests to DataTransformer ones These test check the resulting value of 'createPlayer' function

--- basketapp/tests/Basket/Unit/PlayerToArrayDataTransformerTest.php
+++ basketapp/tests/Basket/Unit/PlayerToArrayDataTransformerTest.php
@@ -47,12 +47,32 @@ class PlayerToArrayDataTransformerTest extends TestCase
 
     public function testAllPropertiesAreTransformedToScalar()
     {
-        $transformedPlayer = $this->dataTransformer->transform($this->player);        
+        $transformedPlayer = $this->dataTransformer->transform($this->player);
         $this->assertSame($this->num, $transformedPlayer['num'], "Data transformer does not fill 'num' with the expected value");
         $this->assertSame(trim($this->label), $transformedPlayer['label'], "Data transformer does not fill 'label' with the expected value");
         $this->assertSame($this->rating, $transformedPlayer['rating'], "Data transformer does not fill 'rating' with the expected value");
         $this->assertSame($this->role, $transformedPlayer['role'], "Data transformer does not fill 'role' with the expected value");
         $this->assertSame($this->time->getTimestamp(), $transformedPlayer['created'], "Data transformer does not fill 'created' with the expected value");
+    }
+
+    public function testCreatePlayerReturnsAPlayer()
+    {
+        $transformedPlayer = $this->dataTransformer->transform($this->player);
+        $player = $this->dataTransformer->createPlayer($transformedPlayer);
+        $this->assertInstanceOf(Player::class,$player,"Data transformer createPlayer function does not return a Player");
+    }
+
+    public function testCreatePlayerReturnsAPlayerWithSameValues()
+    {
+        $transformedPlayer = $this->dataTransformer->transform($this->player);
+        $player = $this->dataTransformer->createPlayer($transformedPlayer);
+        $this->assertSame($this->num, $player->num()->value(), sprintf("Data transformer createPlayer result value of 'num' is not valie, expected %s, got %s",$this->num,$player->num()->value()));
+        $this->assertSame(trim($this->label), $player->label(), sprintf("Data transformer createPlayer result value of 'label' is not valie, expected %s, got %s",$this->label,$player->label()));
+        $this->assertSame($this->rating, $player->rating()->value(), sprintf("Data transformer createPlayer result value of 'rating' is not valie, expected %s, got %s",$this->rating,$player->rating()->value()));
+        $this->assertSame($this->role, $player->role()->value(), sprintf("Data transformer createPlayer result value of 'role' is not valie, expected %s, got %s",$this->role,$player->role()->value()));
+        $this->assertSame($this->time->getTimestamp(), $player->created()->asScalar(), sprintf("Data transformer createPlayer result value of 'created' is not valie, expected %s, got %s",$this->time->format('c'),$player->created()->value()->format('c')));
+
+        
     }
     
 
