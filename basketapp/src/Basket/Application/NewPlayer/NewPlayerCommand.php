29c7978fc87e967ad929a7f4fdb18722ded512d9
Temple <el.temple@gmail.com>
2020-07-09 11:59:13 +0200

    [APPLICATION] MINOR improvements on NewPlayer Command

--- basketapp/src/Basket/Application/NewPlayer/NewPlayerCommand.php
+++ basketapp/src/Basket/Application/NewPlayer/NewPlayerCommand.php
@@ -5,7 +5,6 @@ namespace Basket\Application\NewPlayer;
 use Basket\Application\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -34,14 +33,16 @@ class NewPlayerCommand
         $valid_roles = ['BASE','ESCOLTA','ALERO','ALA-PIVOT','PIVOT'];
 
         $num = $input->getArgument('number');
+        $num = filter_var($num,\FILTER_VALIDATE_INT,["options" => ["min_range" => 0]]);
         $label = $input->getArgument('name');
         $role =  $input->getArgument('position');
         $rating =  $input->getArgument('rating');
+        $rating = filter_var($rating,\FILTER_VALIDATE_INT,["options" => ["min_range" => 0, "max_range" => 100]]);
 
         try {
             assert(is_int($num) && 0<=$num, "Bad number provided, please type any positive integer as player number");
             assert(is_string($label) && !empty($label=trim($label)), "Bad name provided, please type an not empty string for player name");
-            assert(is_string($role) && in_array(strtoupper($role),$valid_roles), "Bad position provided, please choose one of the following:\n".explode(', ',$valid_roles));
+            assert(is_string($role) && in_array(strtoupper($role),$valid_roles), "Bad position provided, please choose one of the following:\n".implode(', ',$valid_roles));
             assert(is_int($rating) && 0<=$rating && 100>=$rating, "Bad rating provided, please type any positive integer between 0 and 100");
 
             $request = new NewPlayerRequest($num, $label, $role, $rating);
@@ -50,7 +51,8 @@ class NewPlayerCommand
             $action = $response->action;
             $output = $this->formatter()->format($response->player);
 
-            $io->success(sprintf('Player has been %s the roster. Now looks like this:', $action==$response::ACTION_ADDED?"added to":"updated on"));
+            $io->success(sprintf('Player has been %s the roster.', $action==$response::ACTION_ADDED?"added to":"updated on"));
+            $io->section(sprintf("Player #%s registered data",$num));
             $io->write($output);
             $io->writeln(["",""]);
 
@@ -59,5 +61,6 @@ class NewPlayerCommand
             return static::FAILURE;
         }
         
+        return static::SUCCESS;
     }
 }
