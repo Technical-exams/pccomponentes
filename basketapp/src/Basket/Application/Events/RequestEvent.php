bfc62cc4de2afccfe17710c312079360efc6c6a0
Temple <el.temple@gmail.com>
2020-07-09 22:35:16 +0200

    [APPLICATION] RequestEvent

---
+++ basketapp/src/Basket/Application/Events/RequestEvent.php
@@ -0,0 +1,36 @@
+<?php namespace Basket\Application\Events;
+
+use Basket\Application\Command\CommandRequest;
+
+class RequestEvent{
+
+    /**
+     * The request triggering the Event
+     * 
+     * @var CommandRequest
+     */
+    protected $request;
+
+    /**
+     * The moment in time when the event was triggered
+     *
+     * @var int
+     */
+    protected $timestamp;
+
+    public function __construct(CommandRequest $request){
+        $this->request = $request;
+        $time = new \DateTimeImmutable();
+        $this->timestamp = $time->getTimestamp();
+    }
+
+    public function request(): CommandRequest
+    {
+        return $this->request;
+    }
+
+    public function timestamp(): int
+    {
+        return $this->timestamp();
+    }
+}
