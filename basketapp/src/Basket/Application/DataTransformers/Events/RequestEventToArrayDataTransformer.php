599fbb2c457bc10c7d94c33c0d9f45e4f86eb86a
Temple <el.temple@gmail.com>
2020-07-09 23:02:06 +0200

    [APPLICATION] RequestEvent to Array DataTransformer

---
+++ basketapp/src/Basket/Application/DataTransformers/Events/RequestEventToArrayDataTransformer.php
@@ -0,0 +1,33 @@
+<?php namespace Basket\Application\DataTransformers\Events;
+
+use Basket\Application\Command\CommandRequest;
+use Basket\Application\DataTransformers\Requests\RequestToArrayDataTransformer;
+use Basket\Application\Events\RequestEvent;
+
+abstract class RequestEventToArrayDataTransformer{
+
+    /**
+     * Request to array transformer
+     *
+     * @var RequestToArrayDataTransformer
+     */
+    protected $request_transformer;
+
+    public function __construct(RequestToArrayDataTransformer $request_transformer){
+        $this->request_transformer = $request_transformer;
+    }
+
+    public function transform(RequestEvent $requestEvent)
+    {
+        $time = new \DateTime('@'.$requestEvent->timestamp());
+        $request_data = $this->requestToArray($requestEvent->request());
+        $event_data = [$time->format('c')];
+        
+        return array_merge($request_data, $event_data);
+    }
+
+    protected function requestToArray(CommandRequest $request): array
+    {
+        return $this->request_transformer->transform($request);
+    }
+}
